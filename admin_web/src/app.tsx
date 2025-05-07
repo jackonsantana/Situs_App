d2831ecc3f9f5461a99d66cf79d210668ef15af9
jackonsantana <jacksonssantanna@gmail.com>
2025-05-07 17:59:16 -0400

    Create app.tsx

---
+++ admin_web/src/app.tsx
@@ -0,0 +1,23 @@
+// admin_web/src/App.tsx
+import { useState, useEffect } from 'react';
+import Login from './Login';
+
+export default function App() {
+  const [auth, setAuth] = useState(false);
+
+  useEffect(() => {
+    const isAuth = localStorage.getItem('auth') === 'true';
+    setAuth(isAuth);
+  }, []);
+
+  const handleLogin = () => setAuth(true);
+
+  if (!auth) return <Login onLogin={handleLogin} />;
+
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Bem-vindo ao Painel do Admin</h1>
+      <p>Aqui é onde seu conteúdo administrativo será exibido.</p>
+    </div>
+  );
+}
