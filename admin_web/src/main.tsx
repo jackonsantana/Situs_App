fd85269a1fa0c2551cd649b2921743e90e5a1b2a
jackonsantana <jacksonssantanna@gmail.com>
2025-05-07 17:59:54 -0400

    Update main.tsx

--- admin_web/src/main.tsx
+++ admin_web/src/main.tsx
@@ -1,1 +1,10 @@
-console.log('Admin web');
+// admin_web/src/main.tsx
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
