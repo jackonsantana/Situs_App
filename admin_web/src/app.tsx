9574eed624477dfdb433e94f987e267487c30a80
jackonsantana <jacksonssantanna@gmail.com>
2025-05-08 01:08:20 -0400

    Update app.tsx

--- admin_web/src/app.tsx
+++ admin_web/src/app.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import ComandoIA from './ComandoIA';
+import Comando from './Comando';
 
 function App() {
   return (
     <div className="App">
-      <ComandoIA />
+      <Comando />
     </div>
   );
 }
