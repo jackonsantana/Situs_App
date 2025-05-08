d03f503f543b330a87c6121530000782aadc4f56
jackonsantana <jacksonssantanna@gmail.com>
2025-05-08 01:57:44 -0400

    Update vite.config.ts

--- admin_web/vite.config.ts
+++ admin_web/vite.config.ts
@@ -2,19 +2,20 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  root: 'src',
+  root: 'src', // 👈 Define a pasta "src" como raiz do projeto
   plugins: [react()],
   build: {
-    outDir: '../dist',
+    outDir: '../dist', // 👈 Gera a pasta dist fora da src
     emptyOutDir: true,
     rollupOptions: {
-      input: path.resolve(__dirname, 'src/index.html'),
-    },
+      input: path.resolve(__dirname, 'src/index.html') // 👈 Caminho corrigido do index
+    }
   },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
-    },
-  },
+      '@': path.resolve(__dirname, 'src') // 👈 Alias para importar arquivos com @
+    }
+  }
 })
