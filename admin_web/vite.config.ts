8974612aa717e0de003c7c87488fedfef24cad26
jackonsantana <jacksonssantanna@gmail.com>
2025-05-08 01:51:32 -0400

    Update vite.config.ts

--- admin_web/vite.config.ts
+++ admin_web/vite.config.ts
@@ -3,10 +3,10 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
+  root: 'src',
   plugins: [react()],
-  root: 'src', // 👈 define que a raiz do projeto está na pasta src
   build: {
-    outDir: '../dist', // 👈 para o dist ir fora de src
+    outDir: '../dist',
     emptyOutDir: true,
     rollupOptions: {
       input: path.resolve(__dirname, 'src/index.html'),
